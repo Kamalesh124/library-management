213caeb253eaa739a0a45aa9c2f03b413819bb0f
Kamalesh124 <k21370794@gmail.com>
2025-01-18 12:54:07 +0530

    allowing link for frontend in backend

--- src/main/java/com/example/CRUDapplication/controller/WebConfig.java
+++ src/main/java/com/example/CRUDapplication/controller/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:5173") // Frontend URL
+                .allowedOrigins("https://library-management-ten-kappa.vercel.app/") // Frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies/authentication if needed
